a240d98ffefc7865013aa38cfa19904bd5477a75
rogue-crypto <84790329+rogue-crypto@users.noreply.github.com>
2025-03-24 18:11:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+app.get('/pastentries', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'pastentries.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
